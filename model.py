7edddcfcf53ba0d5aae3b830ce99aa2324fe23bd
Nikolay Chokoev <nchokoev@gmail.com>
2018-05-15 12:45:25 +0100

    Dropout

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Dropout
 from keras.layers import Convolution2D, Cropping2D
 import sklearn
 import os
@@ -109,12 +109,16 @@ def createNVidiaModel(model):
     """
     model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
     model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Dropout(.2))
     model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
     model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Dropout(.2))
     model.add(Convolution2D(64,3,3, activation='relu'))
     model.add(Flatten())
     model.add(Dense(100))
+    model.add(Dropout(.2))
     model.add(Dense(50))
+    #model.add(Dropout(.1))
     model.add(Dense(10))
     model.add(Dense(1))
 
